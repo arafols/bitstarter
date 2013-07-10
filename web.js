4aeb47d32aff7a7e2f1bf2d5601c354b8b11ae46
Agusti Rafols <agusti.rafols@gmail.com>
2013-07-11 01:14:58 +0200

    Updated web.js reads index.html

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World3!');
+  response.send(buf.toString(fs.readFileSync("index.html",0)));
+  /*response.send('Hello World3!');*/
 });
 
 var port = process.env.PORT || 5000;
