17211cc50f71f8509ca3cae18ba7ca5b9a3fbf35
Agusti Rafols <agusti.rafols@gmail.com>
2013-07-11 01:17:53 +0200

    added buffer declaration

--- web.js
+++ web.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+  var buffer = new Buffer(2048);
   response.send(buf.toString(fs.readFileSync("index.html",0)));
   /*response.send('Hello World3!');*/
 });
